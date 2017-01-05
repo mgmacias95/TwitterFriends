445c7d4341a270ab276e9b1e3c371400d82ca638
mgmacias95 <mgmacias95@correo.ugr.es>
2017-01-05 13:05:10 +0100

    changed url of the view

--- TwitterFriends/TwitterFriends/urls.py
+++ TwitterFriends/TwitterFriends/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^findfriends/', include('findfriends.urls')),
+    url(r'^', include('findfriends.urls')),
     url(r'^admin/', admin.site.urls),
 ]
--- TwitterFriends/findfriends/views.py
+++ TwitterFriends/findfriends/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.template import loader
 from .netjson_serializer import generate_net_json
 
+
 def index(request):
     netjson = generate_net_json()
     template = loader.get_template('findfriends/index.html')
