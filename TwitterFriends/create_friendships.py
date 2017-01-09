15e2e1c483fcd1a1a1a6b6685a6b2fb790a3912e
mgmacias95 <mgmacias95@correo.ugr.es>
2017-01-10 00:04:36 +0100

    modificado para cuando se detenga por error

--- TwitterFriends/create_friendships.py
+++ TwitterFriends/create_friendships.py
@@ -7,14 +7,18 @@ import tweepy
 from findfriends.models import TwitterUser
 
 # function to save frienship on database
-def create_frienship(friends, userid):
+def create_frienship(api, userid):
     user = TwitterUser.objects.get(user_id=userid)
-    print("Saving ", user.screen_name, "'s friends")
-    for friend in friends:
-        if TwitterUser.objects.filter(user_id=friend).exists():
-            u = TwitterUser.objects.get(user_id=friend)
-            print(u.screen_name, " is friend of ", user.screen_name)
-            user.friends.add(u)
+    if len(user.friends.all()) == 0:
+        friends = api.friends_ids(user_id=userid)
+        print("Saving ", user.screen_name, "'s friends")
+        for friend in friends:
+            if TwitterUser.objects.filter(user_id=friend).exists():
+                u = TwitterUser.objects.get(user_id=friend)
+                print(u.screen_name, " is friend of ", user.screen_name)
+                user.friends.add(u)
+    else:
+        print(user.screen_name, "'s friends are already saved")
 
 # Las claves de acceso a twitter están definidas como variables de entorno
 auth = tweepy.OAuthHandler(environ["TWITTER_CONSUMER_KEY"], 
@@ -28,8 +32,8 @@ me = api.me()
 
 # guardamos los amigos del usuario identificado
 amigos = api.friends_ids(user_id=me.id)
-create_frienship(friends=amigos, userid=me.id)
+create_frienship(api=api, userid=me.id)
 # guardamos los amigos de los amigos del usuario identificado
 for amigo in amigos:
     if TwitterUser.objects.filter(user_id=amigo).exists():
-        create_frienship(friends=api.friends_ids(user_id=amigo), userid=amigo)
+        create_frienship(api=api, userid=amigo)
