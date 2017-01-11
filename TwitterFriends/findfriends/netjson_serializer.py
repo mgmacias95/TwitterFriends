7b55a213bbb68dd0b4bae0d31477995d881b9251
Braulio Vargas López <brauliovarlop@correo.ugr.es>
2017-01-11 10:46:37 +0100

    Corregido un error al generar el gdf + adición
    Corregido un error y añadido el poder escoger toda la red completa con todos los usuarios extraídos.

--- TwitterFriends/findfriends/netjson_serializer.py
+++ TwitterFriends/findfriends/netjson_serializer.py
@@ -30,12 +30,12 @@ def generate_net_json():
     return net
 
 # function that generates a gdf file to work on gephi
-def generate_net_gdf():
+def generate_net_gdf(whole_net = False):
     net = []
     # start with nodes
     net.append("nodedef>name VARCHAR,label VARCHAR,verified BOOLEAN,location VARCHAR")
     u = TwitterUser.objects.first()
-    net.append('{},{},{},{}'.format(u.user_id, u.screen_name, 
+    net.append('{},{},{},\'{}\''.format(u.user_id, u.screen_name, 
             u.is_verified, u.location))
 
     for user in u.friends.all():
@@ -47,11 +47,17 @@ def generate_net_gdf():
     
     for friend in u.friends.all():
         net.append('{},{},true'.format(u.user_id, friend.user_id))
-
-    for user in u.friends.all():
-        # we get the friends in common
-        common_f = user.friends.filter(friends__in=u.friends.all()).distinct()
-        for friend in common_f:
-            net.append('{},{},true'.format(user.user_id, friend.user_id))
+    
+    if not whole_net:
+        for user in u.friends.all():
+            # we get the friends in common
+            common_f = user.friends.filter(friends__in=u.friends.all()).distinct()
+            for friend in common_f:
+                net.append('{},{},true'.format(user.user_id, friend.user_id))
+    else:
+        for user in u.friends.all():
+            # we take the whole net with all users and links
+            for friend in user.friends.all():
+                net.append('{},{},true'.format(user.user_id, friend.user_id))
 
     return '\n'.join(net)
